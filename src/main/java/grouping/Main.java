8944742c73ca551e7f1af850aa831133916fcd73
HoseinRezaeeM <rezaeen77@gmail.com>
2023-10-22 16:35:08 +0330

    edit  PracticeStream Class and main

--- src/main/java/grouping/Main.java
+++ src/main/java/grouping/Main.java
@@ -1,14 +1,26 @@
 package grouping;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner =new Scanner(System.in);
 
-        System.out.println("********* Hosein Rezaei Q2-HW11 *********");
-        PracticeStream.scannerNumer();
-        System.out.println("------- WordByLenght ------");
-        PracticeStream.wordByLength();
-        System.out.println("--- GroupingWordByLength ---");
-        PracticeStream.groupingWordByLength();
+        System.out.println("********* Hosein Rezaei Q2-HW11 *********\n");
+        System.out.println("How Many Enter Word ?");
+        int count =scanner.nextInt();
+        System.out.println("Please Enter Word: ");
+            String[] sc =new String[count];
+            for ( int i = 0; i < count; i++) {
+                sc[i] = scanner.next();
+            }
+
+
+
+        System.out.println("------- WordByLength -------\n");
+        System.out.println(PracticeStream.wordByLength(sc)+"\n");
+        System.out.println("--- GroupingWordByLength ---\n");
+        System.out.println(PracticeStream.groupingWordByLength(sc));
 
     }
 }
--- src/main/java/grouping/PracticeStream.java
+++ src/main/java/grouping/PracticeStream.java
@@ -2,34 +2,23 @@ package grouping;
 
 import java.util.Arrays;
 import java.util.Map;
-import java.util.Scanner;
+
 import java.util.Set;
 import java.util.stream.Collectors;
 
 public class PracticeStream {
 
-    public static String[] scannerNumer() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("--How many enter word ?");
-        int n = scanner.nextInt();
-        System.out.println("--Please Enter Word :");
-        String[] sc = new String[n];
-        for (int i = 0; i < n; i++) {
-            sc[i] = scanner.nextLine();
-        }
-        return sc;
-    }
 
-    public static Map<String, Integer> wordByLength() {
+    public static Map<String, Integer> wordByLength(String[] word) {
         Map<String, Integer> map;
-        map = Arrays.stream(scannerNumer())
+        map = Arrays.stream(word)
                 .collect(Collectors.toMap(String::new, String::length));
         return map;
     }
 
-    public static Map<Integer, Set<String>> groupingWordByLength() {
+    public static Map<Integer, Set<String>> groupingWordByLength(String[] word) {
         Map<Integer, Set<String>> integerSetMap;
-        integerSetMap = Arrays.stream(scannerNumer())
+        integerSetMap = Arrays.stream(word)
                 .collect(Collectors.groupingBy(String::length, Collectors.mapping(String::new, Collectors.toSet())));
         return integerSetMap;
     }
