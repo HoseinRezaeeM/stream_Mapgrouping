b0eb6bd8392b988292762a7f5eda701a2d16c0e2
HoseinRezaeeM <rezaeen77@gmail.com>
2023-10-22 16:46:12 +0330

    scanner main change to PracticeStream class and Create Method

--- src/main/java/grouping/Main.java
+++ src/main/java/grouping/Main.java
@@ -1,20 +1,13 @@
 package grouping;
 
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner =new Scanner(System.in);
 
         System.out.println("********* Hosein Rezaei Q2-HW11 *********\n");
-        System.out.println("How Many Enter Word ?");
-        int count =scanner.nextInt();
-        System.out.println("Please Enter Word: ");
-            String[] sc =new String[count];
-            for ( int i = 0; i < count; i++) {
-                sc[i] = scanner.next();
-            }
 
+
+        String[] sc =PracticeStream.scannerWords();
 
 
         System.out.println("------- WordByLength -------\n");
--- src/main/java/grouping/PracticeStream.java
+++ src/main/java/grouping/PracticeStream.java
@@ -3,10 +3,23 @@ package grouping;
 import java.util.Arrays;
 import java.util.Map;
 
+import java.util.Scanner;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 public class PracticeStream {
+
+    public static String[] scannerWords() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("How Many Enter Word ?");
+        int count = scanner.nextInt();
+        System.out.println("Please Enter Word: ");
+        String[] sc = new String[count];
+        for (int i = 0; i < count; i++) {
+            sc[i] = scanner.next();
+        }
+        return sc;
+    }
 
 
     public static Map<String, Integer> wordByLength(String[] word) {
@@ -16,10 +29,12 @@ public class PracticeStream {
         return map;
     }
 
+
     public static Map<Integer, Set<String>> groupingWordByLength(String[] word) {
         Map<Integer, Set<String>> integerSetMap;
         integerSetMap = Arrays.stream(word)
                 .collect(Collectors.groupingBy(String::length, Collectors.mapping(String::new, Collectors.toSet())));
         return integerSetMap;
     }
+
 }
