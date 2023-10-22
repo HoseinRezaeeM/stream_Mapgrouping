6ec04b21b6ff6c05e725f919b765d98c6ec0a549
HoseinRezaeeM <rezaeen77@gmail.com>
2023-10-22 15:39:11 +0330

    Write Main Class and declared

---
+++ src/main/java/grouping/Main.java
@@ -0,0 +1,14 @@
+package grouping;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("********* Hosein Rezaei Q2-HW11 *********");
+        PracticeStream.scannerNumer();
+        System.out.println("------- WordByLenght ------");
+        PracticeStream.wordByLength();
+        System.out.println("--- GroupingWordByLength ---");
+        PracticeStream.groupingWordByLength();
+
+    }
+}
