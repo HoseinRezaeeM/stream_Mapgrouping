f90037f36dfacdc7033761e40fe1ec0ca2d54589
HoseinRezaeeM <rezaeen77@gmail.com>
2023-10-22 15:35:48 +0330

    edit PracticeStream Class

--- src/main/java/grouping/PracticeStream.java
+++ src/main/java/grouping/PracticeStream.java
@@ -8,28 +8,29 @@ import java.util.stream.Collectors;
 
 public class PracticeStream {
 
-    public static String[] scannerNumer(){
-        Scanner scanner =new Scanner(System.in);
+    public static String[] scannerNumer() {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("--How many enter word ?");
         int n = scanner.nextInt();
         System.out.println("--Please Enter Word :");
-        String[] sc =new String[n];
+        String[] sc = new String[n];
         for (int i = 0; i < n; i++) {
-            sc[i] =scanner.nextLine();
+            sc[i] = scanner.nextLine();
         }
         return sc;
     }
 
-    public Map<String,Integer> wordByLength(){
+    public Map<String, Integer> wordByLength() {
         Map<String, Integer> map;
         map = Arrays.stream(scannerNumer())
-                .collect(Collectors.toMap(String::new,String::length));
+                .collect(Collectors.toMap(String::new, String::length));
         return map;
     }
-    public Map<Integer, Set<String>> groupingWordByLength(){
+
+    public Map<Integer, Set<String>> groupingWordByLength() {
         Map<Integer, Set<String>> integerSetMap;
         integerSetMap = Arrays.stream(scannerNumer())
-                .collect(Collectors.groupingBy(String::length,Collectors.mapping(String::new,Collectors.toSet())));
+                .collect(Collectors.groupingBy(String::length, Collectors.mapping(String::new, Collectors.toSet())));
         return integerSetMap;
     }
 }
