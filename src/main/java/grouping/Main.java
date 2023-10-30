2593bcfe7bc47cec40b97b81ec3c717fbe199cff
HoseinRezaei <rezaeen77@gmail.com>
2023-10-30 23:10:09 +0330

    write try-catch

--- src/main/java/grouping/Main.java
+++ src/main/java/grouping/Main.java
@@ -5,19 +5,23 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("********* Hosein Rezaei Q2-HW11 *********\n");
+        while (true){
+        try {
+            String[] sc = PracticeStream.scannerWords();
+          System.out.println("------- WordByLength -------\n");
+            System.out.println(PracticeStream.wordByLength(sc) + "\n");
+            System.out.println("--- GroupingWordByLength ---\n");
+            System.out.println(PracticeStream.groupingWordByLength(sc) + "\n");
+            System.out.println("--- groupingWordBySum ----\n");
+            PracticeStream.groupingWordBySum(sc);
+            break;
+        } catch (Exception e) {
+            System.out.println("invalid Enter!!! ");
+        }
 
 
-        String[] sc =PracticeStream.scannerWords();
-
-
-        System.out.println("------- WordByLength -------\n");
-        System.out.println(PracticeStream.wordByLength(sc)+"\n");
-        System.out.println("--- GroupingWordByLength ---\n");
-        System.out.println(PracticeStream.groupingWordByLength(sc)+"\n");
-        System.out.println("--- groupingWordBySum ----\n");
-        PracticeStream.groupingWordBySum(sc);
 
 
 
     }
-}
+}}
